df338bd69221a62812113fdf116b4ba392a6c5fc
Marek Małek <malekmarek56@gmail.com>
2026-04-15 03:15:41 +0200

    feat: add lab3 setup scripts

---
+++ common/sql/adventure-works/mssql_advworks_ddl_data.sql
@@ -0,0 +1,30 @@
+USE master;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'adventureworksStripped')
+BEGIN
+    CREATE DATABASE adventureworksStripped;
+END
+GO
+
+USE adventureworksStripped;
+GO
+
+DROP TABLE IF EXISTS salesorderheader;
+DROP TABLE IF EXISTS salesorderdetail;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS address;
+DROP TABLE IF EXISTS person;
+GO
+
+-- 3. Copy all required tables from AdventureWorks
+SELECT * INTO [salesorderheader] FROM [AdventureWorks2017].sales.[salesorderheader];
+SELECT * INTO [salesorderdetail] FROM [AdventureWorks2017].sales.[salesorderdetail];
+
+SELECT * INTO customer FROM [AdventureWorks2017].sales.customer;
+
+SELECT * INTO address FROM [AdventureWorks2017].person.address;
+
+SELECT businessentityid, persontype, namestyle, title, firstname, middlename, lastname, suffix, emailpromotion, rowguid, modifieddate
+INTO person FROM [AdventureWorks2017].person.person;
+GO
